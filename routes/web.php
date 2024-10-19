1d89b5bb423958105013541eeb06b26cae06b4d1
lanceberge <bergeron.lance6@gmail.com>
2024-10-19 13:21:07 -0400

    fix

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AppController;
 
-Route::get('/', function () {
-  return view('welcome');
-});
-
 Route::get('/', [AppController::class, 'index']);
